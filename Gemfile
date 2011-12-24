b28c224d99cb401e8874f377508cdea1bcceb568
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-12-24 11:08:06 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :test do
+  gem 'turn', :require => false
+end
+
+group :development, :test do
+  gem "rspec-rails", "~> 2.7"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JacobsQuiz::Application.config.secret_token = '7fc3d3e75ec2c1b3f328c5dc38c2212547ce73cf2cf9d2cdce7a755a1bf3aec7c371ffc305793171ad5f9c0faa5a7689d9fd8892f488e8e59add980ea0a599f1'
