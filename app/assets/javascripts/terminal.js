f8374a6d41d9734068428b5947b4220f198cfed0
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-12-24 20:15:00 -0500

    adding more to the terminal

--- app/assets/javascripts/terminal.js
+++ app/assets/javascripts/terminal.js
@@ -37,22 +37,33 @@ function termHandler() {
     this.newLine();
     var parser = new Parser();
     parser.parseLine(this);
-    var command = this.argv[this.argc++];
-    
-    if (command.match(/^\s*exit\s*$/i)) {
-        this.close();
-        return;
-    }
-    else if (command === 'git') {
-        var subCommand = this.argv[this.argc++];
-        if (subCommand === 'status') {
-            this.write('# On branch master\n# Changes not staged for commit:\n#   (use "git add <file>..." to update what will be committed)\n#   (use "git checkout -- <file>..." to discard changes in working directory)\n#\n#	modified:   app/views/home/index.haml\n#\n# Untracked files:\n#   (use "git add <file>..." to include in what will be committed)\n#\n#	app/assets/javascripts/terminal.js\n#	public/assets/\nno changes added to commit (use "git add" and/or "git commit -a")\n')
+
+    if (this.argv.length > 0) {
+        var command = this.argv[this.argc++];
+        
+        if (command === 'exit' || command === 'quit') {
+            this.close();
+            return;
         }
-    }
-    else if (this.lineBuffer != '') {
-        // echo with write for wrapping, but escape any mark-up
-        this.write('You wrote: '+this.lineBuffer.replace(/%/g, '%%'));
-        this.newLine();
+        else if (command === 'clear') {
+            this.clear();
+        }
+        else if (command === 'git') {
+            var subCommand = this.argv[this.argc++];
+            var flag = this.argv[this.argc++];
+            if (subCommand === 'status') {
+                this.write('# Changes not staged for commit:\n#   (use "git add <file>..." to update what will be committed)\n#   (use "git checkout -- <file>..." to discard changes in working directory)\n#\n#	modified:   Gemfile\n#\nno changes added to commit (use "git add" and/or "git commit -a")\n')
+            }
+
+            if (subCommand === 'add' && flag == '-p') {
+                this.write('diff --git a/Gemfile b/Gemfile\nindex 3651937..27ffdb5 100644\n--- a/Gemfile\n+++ b/Gemfile\n@@ -13,7 +13,7 @@ gem "devise", "1.5.3"\n gem "haml", "3.1.4"\n gem "heroku", "2.17.0"\n # To use debugger\n-# gem "ruby-debug19", :require => "ruby-debug"\n+gem "ruby-debug19", :require => "ruby-debug"\n \n group :test do\n   gem "turn", :require => false\nStage this hunk [y,n,q,a,d,/,j,J,g,e,?]? n\nn\n@@ -22,6 +22,7 @@ end\n group :development, :test do\n   gem "rspec-rails", "~> 2.7"\n   gem "sqlite3", "1.3.5"\n+  gem "webrat"\n end\n \n group :production do\nStage this hunk [y,n,q,a,d,/,K,g,e,?]?')
+            }
+        }
+        else if (this.lineBuffer != '') {
+            // echo with write for wrapping, but escape any mark-up
+            this.write('You wrote: '+this.lineBuffer);
+            this.newLine();
+        }
     }
     this.prompt();
 }
